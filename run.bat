9026c45206377870f241f63012b182b5c8b9cf2a
yq9721 <497445109@qq.com>
2021-01-29 00:47:31 +0800

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
-javac src/artsploit/AwesomeScriptEngineFactory.java -cp ./lib2/*
-javac src/artsploit/Tunnel.java -cp ./lib2/*
-javac src/artsploit/GameInfo.java -cp ./lib2/*
-jar -cvf yaml-payload.jar -C src/ .
+javac src/artsploit/AwesomeScriptEngineFactory.java -cp ./lib/*
+javac src/artsploit/Tunnel.java -cp ./lib/*
+javac src/artsploit/GameInfo.java -cp ./lib/*
+jar -cvfm yaml-payload.jar src/META-INF/MANIFEST.MF -C src/ .
